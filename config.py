91640c17b790a57892814545d67d0f0cba7d33ff
Arif <arif.rahman@wcma.vic.gov.au>
2025-08-07 14:11:41 +1000

    done

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ assistant_id = st.secrets["ASSISTANT_ID"]
 
 station_name = "Dooen 2"
 
-base_csv_path = "more_data/all_small.csv"
+base_csv_path = "more_data/all.csv"
 filtered_csv = "more_data/filtered.csv"
 station_csv_path = "more_data/portal_site_list.csv"
